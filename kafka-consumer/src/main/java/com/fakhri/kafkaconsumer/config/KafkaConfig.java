926a994515baafa4d701459a078b77007d89cb5b
fakhrichaerul <fakhrichaerul@gmail.com>
2021-04-05 19:01:08 +0700

    update KafkaConfig

--- kafka-consumer/src/main/java/com/fakhri/kafkaconsumer/config/KafkaConfig.java
+++ kafka-consumer/src/main/java/com/fakhri/kafkaconsumer/config/KafkaConfig.java
@@ -23,8 +23,8 @@ public class KafkaConfig {
 
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
